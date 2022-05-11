3a89b729d407bbb8ba300bd7c7cdd734a77ac0a1
Enrico-N <ngenricok@gmail.com>
2022-05-10 18:13:23 -0700

    Commit Linkedin Changes

--- src/App.js
+++ src/App.js
@@ -14,15 +14,7 @@ function App() {
         <h1>Hello, I'm Enrico</h1>
         <h2>Software Developer</h2>
         
-        <i class="fa-brands fa-linkedin" href="https://www.linkedin.com/in/enrico-ng/">
-        <a
-          className="Linkedin-link"
-          href= ""
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-
-        </a>
+        <i class="fa-brands fa-linkedin" className="Linkedin-link" href="https://www.linkedin.com/in/enrico-ng/" target="_blank">
         </i>
         
       </header>

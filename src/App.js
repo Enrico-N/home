5a93f0673b9e0daed693d4e0ebdfd9ef1c7233dc
Enrico-N <ngenricok@gmail.com>
2022-05-10 21:39:12 -0700

    Update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import linkedin from './linkedin-brands.svg';
 import './App.css';
 import './FontAwesomeIcons';
 import React, {useState} from 'react';
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
@@ -19,7 +19,7 @@ function App() {
         <h1>Hello, I'm Enrico</h1>
         <h2>Software Developer</h2>
         
-        <FontAwesomeIcon icon={faLinkedin} href="https://www.linkedin.com/in/enrico-ng/" target="_blank" />
+        <FontAwesomeIcon icon={faLinkedin} className="Linkedin-link" href="https://www.linkedin.com/in/enrico-ng/" target="_blank" />
         
         
       </header>

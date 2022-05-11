0f1669d8cfd1aa08e56543e64026c99847123faf
Enrico-N <ngenricok@gmail.com>
2022-05-10 18:37:18 -0700

    Linkedin Img

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import linkedin from './linkedin-brands.svg';
 import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React, {useState} from 'react';
@@ -15,7 +16,7 @@ function App() {
         <h1>Hello, I'm Enrico</h1>
         <h2>Software Developer</h2>
         
-        <FontAwesomeIcon icon="fa-brands fa-linkedin" className="Linkedin-link" href="https://www.linkedin.com/in/enrico-ng/" target="_blank"/>
+        <img src={linkedin} className="Linkedin-link" href="https://www.linkedin.com/in/enrico-ng/" target="_blank"></img>
         
         
       </header>

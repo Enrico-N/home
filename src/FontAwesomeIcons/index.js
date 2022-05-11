cdc0a61ad31e8fdad010177bc6c8b2e256ccbe19
Enrico-N <ngenricok@gmail.com>
2022-05-10 21:28:31 -0700

    Update Font Library

--- src/FontAwesomeIcons/index.js
+++ src/FontAwesomeIcons/index.js
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
-import { fa } from '@fortawesome/free-brands-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 
-library.add(fas, fa, FontAwesomeIcon)
+library.add(fas, faLinkedin, faGithub, FontAwesomeIcon)

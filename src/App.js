8e21c57ecc240fb0b251688a78439ebde0de4868
Enrico-N <ngenricok@gmail.com>
2022-05-10 18:05:12 -0700

    Import FaCoffee

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faCoffee} from '@fortawesome/free-solid-svg-icons';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
 
 function App() {
   return (

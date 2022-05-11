efa9d81cd4685ef3781300423b3720d6cbeb80cc
Enrico-N <ngenricok@gmail.com>
2022-05-10 18:51:30 -0700

    Linkedin Commit

--- src/FontAwesomeIcons/index.js
+++ src/FontAwesomeIcons/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
-import { faTwitter, faFontAwesome, falinkedin} from '@fortawesome/free-brands-svg-icons'
+import { faTwitter, faFontAwesome, faLinkedin} from '@fortawesome/free-brands-svg-icons'
 
 
-library.add(fas, faTwitter, faFontAwesome, falinkedin)
+library.add(fas, faTwitter, faFontAwesome, faLinkedin)

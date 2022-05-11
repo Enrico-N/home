81d28893329bbfa40cd948a9a3d312bc41f1be32
Enrico-N <ngenricok@gmail.com>
2022-05-10 17:30:44 -0700

    Small Commit

--- src/App.js
+++ src/App.js
@@ -9,14 +9,8 @@ function App() {
         <h1>Hello, I'm Enrico</h1>
         <h2>Software Developer</h2>
         
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+
       </header>
     </div>
   );
